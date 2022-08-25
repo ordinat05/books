a23120e6089ad0305b41d1a5b7220e5d24a8462a
sebahattin <sebkucuk05@gmail.com>
2022-08-25 06:07:41 +0300

    d4

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,25 +1,32 @@
 import React from "react";
 import "./BookList.css";
 import Book from "./Book";
+import { BookContext } from "../contexts/BookContext";
 
 class BookList extends React.Component {
   render() {
-    // console.log(this.props.kitaplar);
-    const bookList = this.props.books.map((book, i) => {
-      return <Book book={book} key={i} />;
-    });
     return (
-      <section className="page-section bg-light" id="portfolio">
-        <div className="container">
-          <div className="text-center">
-            <h2 className="section-heading text-uppercase">BookFolio</h2>
-            <h3 className="section-subheading text-muted">
-              Lorem ipsum dolor sit amet consectetur.
-            </h3>
-          </div>
-          <div className="row">{bookList}</div>
-        </div>
-      </section>
+      <BookContext.Consumer>
+        {(value) => {
+          return (
+            <section className="page-section bg-light" id="portfolio">
+              <div className="container">
+                <div className="text-center">
+                  <h2 className="section-heading text-uppercase">BookFolio</h2>
+                  <h3 className="section-subheading text-muted">
+                    Lorem ipsum dolor sit amet consectetur.
+                  </h3>
+                </div>
+                <div className="row">
+                  {value.books.map((book, i) => {
+                    return <Book book={book} key={i} />;
+                  })}
+                </div>
+              </div>
+            </section>
+          );
+        }}
+      </BookContext.Consumer>
     );
   }
 }
